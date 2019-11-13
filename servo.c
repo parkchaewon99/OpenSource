dd4d83554041062c6216032ca01c0943db57a60f
parkchaewon99 <psy980a1@naver.com>
2019-11-13 16:00:18 +0900

    servo

---
+++ servo.c
@@ -0,0 +1,24 @@
+import RPI.GPIO as GPIO
+import time
+
+pin = 18
+
+GPIO.setmode(GPIO.BMC)
+GPIO.setup(pin,GPIO.OUT)
+p=GPIO.PWM(pin, 50)
+p.start(0)
+cnt=0
+try:
+	while True:
+		p.ChangeDutyCycle(1)
+		print "angle : 1"
+		time.sleep(1)
+		p.ChangeDutyCycle(5)
+		print"angle : 5"
+		time.sleep(1)
+		p.ChangeDutyCycle(8)
+		print("angle : 8")
+		time.sleep(1)
+except KeyboardInterrupt:
+	p.stop()
+GPIO.cleanup()
